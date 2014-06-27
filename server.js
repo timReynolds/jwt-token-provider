1287335fd2928fa7f2bda862a42dfbce19b2497c
timReynolds <tim@fundapps.co>
2014-06-27 12:54:40 +0100

    Added jwt token server code

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require('express')
+ , fs = require('fs')
+ , bodyParser = require('body-parser')
+ , port = process.env.PORT || 1546
+ , jwt = require('jsonwebtoken')
+ , config = require('./config.json')
+ , app = express();
+
+var cert = fs.readFileSync(config.cert)
+ , certOptions = {algorithm: 'RS256', expiresInMinutes: 30}
+ , certPayload = {}
+ , response = {};
+
+if(!config) {
+  console.error('Failed to load config')
+  process.exit(1)
+}
+
+app.use(bodyParser());
+app.use(function(req, res, next){
+  	console.log('%s %s', req.method, req.url);
+  	next();
+});
+
+app.use(function(req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    if (req.method && 'OPTIONS' == req.method) {
+    	res.send(200);
+    }
+    next();
+});
+
+app.post('/',actionLogin);
+app.post('/login',actionLogin);
+
+function actionLogin(req, res) {
+	certPayload = req.body;
+	certOptions.audience = req.body.app;
+	response.token = jwt.sign(certPayload, cert, certOptions);
+	res.send(response);
+}
+
+var server = app.listen(port, function() {
+    console.log('Listening on port %d', server.address().port);
+});
