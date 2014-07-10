a7fd13318acd118b1445ce2aec719129e2d96ea4
Tim Reynolds <tim@timothyreynolds.co.uk>
2014-07-10 10:12:54 +0100

    Updated format of server.js
    2 spaces are the future

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express')
  , fs = require('fs')
  , bodyParser = require('body-parser')
- , port = process.env.PORT || 1546
  , jwt = require('jsonwebtoken')
  , _ = require('underscore')
+ , port = process.env.PORT || 1546
  , config = require('./config.json')
  , app = express();
 
@@ -19,20 +19,18 @@ if(!config) {
 
 app.use(bodyParser());
 app.use(function(req, res, next){
-  	console.log('%s %s', req.method, req.url);
-  	next();
+  console.log('%s %s', req.method, req.url);
+  next();
 });
 
 app.use(function(req, res, next) {
-
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    if (req.method && 'OPTIONS' == req.method) {
-    	res.send(200);
-    }
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  if (req.method && 'OPTIONS' == req.method) {
+  	res.send(200);
+  }
+  next();
 });
 
 app.post('/',actionLogin);
@@ -40,11 +38,11 @@ app.post('/login',actionLogin);
 
 function actionLogin(req, res) {
   certPayload = _.extend(certPayload, req.body);
-	certOptions.audience = req.body.app;
-	response.token = jwt.sign(certPayload, cert, certOptions);
-	res.send(response);
+  certOptions.audience = req.body.app;
+  response.token = jwt.sign(certPayload, cert, certOptions);
+  res.send(response);
 }
 
 var server = app.listen(port, function() {
-    console.log('Listening on port %d', server.address().port);
+  console.log('Listening on port %d', server.address().port);
 });
